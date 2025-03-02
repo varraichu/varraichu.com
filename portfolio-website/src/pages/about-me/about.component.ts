05b104651ec908b27537291e7d72cf70ef810299
varraichu <qasimanwarv5@gmail.com>
2025-03-03 00:02:50 +0500

    resume + idl

--- portfolio-website/src/pages/about-me/about.component.ts
+++ portfolio-website/src/pages/about-me/about.component.ts
@@ -5,4 +5,20 @@ import { Component } from "@angular/core";
     templateUrl: './about.component.html',
 })
 
-export class AboutMe {}
+export class AboutMe {
+    private audio = new Audio();
+
+    constructor() {
+        this.audio.src = 'assets/idl.mp3';
+        this.audio.load();
+    }
+
+    playSong() {
+        if (this.audio.paused) {
+            this.audio.play();
+        } else {
+            this.audio.pause();
+            this.audio.currentTime = 0;
+        }
+    }
+}
